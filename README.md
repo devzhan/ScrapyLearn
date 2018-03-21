183bcde63f6fe410593cae1c6c3462685caf6d16
Owen <zhxiuwen@163.com>
2018-03-21 23:33:55 +0800

    爬取horocn的ip地址

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ scrapy学习
 # 1.scrapy 爬取妹子图http://www.mzitu.com/
 # 2.scrapy 爬取http://www.66ip.cn/1.html 的ip代理
 # 3.爬取https://proxy.coderbusy.com/ 的ip代理
+# 4.爬取https://proxy.horocn.com/free-proxy.html 的ip代理
---
+++ proxy/proxy/spiders/horocn.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# __author__: owen_zhan
+
+import scrapy
+from proxy.items import ProxyPoolItem
+from bs4 import BeautifulSoup
+
+
+class CoderBusySpider(scrapy.Spider):
+    name = 'horocn'
+    allowed_domains = ['horocn.com']
+
+    start_urls = [
+        'https://proxy.horocn.com/free-proxy.html',
+    ]
+
+    def parse(self, response):
+        soup = BeautifulSoup(response.text, "html.parser")
+        itmes = soup.find_all("tbody")
+        trs = itmes[0].find_all('tr')
+        print(trs)
+        for tr in trs:
+            ips = tr.find_all('th')
+            ip = ips[0].text.strip()
+            protocols = "HTTP"
+            ports = ips[1].text
+            address = ips[2].text
+            time = ips[3].text
+            yield ProxyPoolItem({
+                'ip': ip,
+                'protocol': protocols,
+                'port': ports,
+                'types': '',
+                'address': address,
+                'website': 'www.proxy.horocn.com/',
+                'time': time
+            })
